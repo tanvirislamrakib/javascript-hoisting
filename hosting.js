4f7cb0284e2e3879fb584dd0f78693897438c413
tanvirislamrakib <tanvirislamrakib93@gmail.com>
2024-01-10 10:07:38 +0600

    first commit

---
+++ hosting.js
@@ -0,0 +1,14 @@
+var yourname = "Tanvir"; // Declarations
+console.log(yourname);
+
+yourname = "Hablu Mama" // Assign
+var yourname; // declear
+console.log(yourname);
+
+number = 100;
+var number;
+console.log(number);
+
+let myname = "rakib";
+myname = "tanvir";
+console.log(myname);
